886c205bb9fd545ff0dfb5d5cee8191e4be3a84d
Stephan Ullmann <stephan.ullmann@posteo.de>
2023-06-28 14:57:00 +0200

    added cors and chrome preflight response headers

--- server.js
+++ server.js
@@ -8,26 +8,32 @@ const PORT = process.env.PORT || 8080;
 
 const app = express();
 
-// const whitelist = ["http://localhost:3000", "https:our-nice-deployment.com"];
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (whitelist.indexOf(origin) !== -1) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("Haha"));
-//     }
-//   },
-// };
-app.use(cors());
+const whitelist = ["http://localhost:3000", "https://sightseeker.netlify.app"];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Haha"));
+    }
+  },
+};
+
+app.use(function setCommonHeaders(req, res, next) {
+  res.set("Access-Control-Allow-Private-Network", "true");
+  next();
+});
+
+app.options("*", cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 connectDB();
 
-app.get("/", (req, res) => {
+app.get("/", cors(corsOptions), (req, res) => {
   res.send("Welcome");
 });
-app.use("/user", userRoutes);
+app.use("/user", cors(), userRoutes);
 
 app.listen(PORT, () => {
   console.log("Running".rainbow);
