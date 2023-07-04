1ae0140388604537d75848dead0c5010f4b0a0f7
Adam <adamna1989@gmail.com>
2023-07-05 00:55:29 +0100

    added chat renaming func

--- routes/ChatRoute.js
+++ routes/ChatRoute.js
@@ -1,12 +1,13 @@
 const express = require('express')
-const { createChat, userChats, findChat, createNewChat, deleteChat } = require('../controllers/chatController')
+const { /* createChat, */ userChats, findChat, createNewChat, deleteChat, editChat } = require('../controllers/chatController')
 
 const app = express.Router()
 
 app.post("/chat", createNewChat)
-app.post("/chat", createChat)
+/* app.post("/chat", createChat) */
 app.get("/chat/:userId", userChats)
 app.get("/chat/find/:firstId/:secondId", findChat)
 app.delete("/", deleteChat)
+app.put("/", editChat)
 
 module.exports = app
